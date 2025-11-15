c4f077fc75963f1170b3e5812e1b4f684c9ad786
user <user@DESKTOP-8P331MO>
2025-11-14 19:16:39 -0500

    Add project files.

---
+++ API.W.MOVIES_2/DAL/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using API.W.MOVIES_2.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+namespace API.W.MOVIES_2.DAL
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ API.W.MOVIES_2/DAL/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.W.MOVIES_2.DAL.Models
+{
+    public class Category : AuditBase
+    {
+        [Required]
+        [Display(Name = "Nombre de la categoria")]
+        public string Name { get; set; }
+    }
+}
